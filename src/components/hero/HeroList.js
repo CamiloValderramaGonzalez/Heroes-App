7228cbb9ad5c63901dce3362572667c9a291c26f
Camilo Valderrama <camilovalderrama@gmail.com>
2022-04-11 12:02:54 -0300

    Heroes App

---
+++ src/components/hero/HeroList.js
@@ -0,0 +1,16 @@
+import { useMemo } from "react";
+import { getHeroesBuPublisher } from "../../selectors/getHeroesByPublisher"
+import { HeroCard } from "./HeroCard";
+
+export const HeroList = ({ publisher }) => {
+    const heroes = useMemo(() => getHeroesBuPublisher(publisher), [publisher]);
+    return (
+        <div className="row row-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeIn">
+            {
+                heroes.map(hero => (
+                    <HeroCard key={hero.id} {...hero} />
+                ))
+            }
+        </div >
+    )
+}
