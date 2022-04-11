0c342463c79072df6f837a7ebc93ca0386aef797
Camilo Valderrama <camilovalderrama@gmail.com>
2022-04-11 14:02:10 -0300

    manejo de imagenes

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { heroImages } from "../../helpers/heroImages";
 import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
@@ -25,13 +26,11 @@ export const HeroScreen = () => {
         characters
     } = hero
 
-    const imagePath = `/assets/${id}.jpg`
-
     return (
         <div className="row mt-5">
             <div className="col-4">
                 <img
-                    src={imagePath}
+                    src={heroImages(`./${id}.jpg`)}
                     alt={superhero}
                     className="img-thumbnail animate__animated animate__fadeInLeft"
                 />
